94a85f560ddec0ac0394b8e5a6cfd1e97159a8d7
sheroline <201867087@ump.ac.za>
2021-05-14 15:49:26 +0200

    my file

---
+++ mul.java
@@ -0,0 +1,12 @@
+public class {
+public static void main(String[]args){
+int num1;
+int num2;
+int answer;
+Scanner in =new Scanner(System.in);
+num1=in.nextInt();
+num2=in.nextInt();
+answer=num1*num2;
+System.out.println(+answer);
+}
+}
